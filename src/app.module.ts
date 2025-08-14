b7cfb86640a26aac206b18b34a5927cc4fd2fbbc
gitdevjin <dev.hjshin@gmail.com>
2025-08-14 17:19:39 -0400

    database configuration for local postgres added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get('database');
+        return {
+          type: 'postgres',
+          url: dbConfig.url,
+          ssl: dbConfig.ssl,
+          synchronize: dbConfig.synchronize,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
