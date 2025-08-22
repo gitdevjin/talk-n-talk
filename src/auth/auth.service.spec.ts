332e815001cca5fcae4d811ec45296d3a29cf359
gitdevjin <dev.hjshin@gmail.com>
2025-08-21 20:11:34 -0400

    TestCode for authService added

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,98 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { BadRequestException } from '@nestjs/common';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('AuthService Testing', () => {
+  let authService: AuthService;
+  let mockUserService: Partial<Record<keyof UserService, jest.Mock>>;
+  let mockJwtService: Partial<JwtService>;
+  let mockConfigService: Partial<ConfigService>;
 
   beforeEach(async () => {
+    mockUserService = {
+      getUserByEmail: jest.fn(),
+      createUser: jest.fn(),
+      updateRefreshToken: jest.fn(),
+    };
+
+    mockJwtService = { sign: jest.fn() };
+
+    mockConfigService = {
+      get: jest.fn((key: string) => {
+        if (key === 'jwt') {
+          return { secret: 'secret-string', accessTokenTtl: '1h', refreshTokenTtl: '7d' };
+        }
+
+        if (key === 'auth') {
+          return { hashRounds: 10 };
+        }
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('authService should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+
+  describe('extractTokenFromHeader function', () => {
+    it('bearer token should be extracted correctly', () => {
+      const scheme = 'bearer';
+      const token = '1234#1234$';
+      const result = authService.extractTokenFromHeader(`${scheme} ${token}`, 'bearer');
+      expect(result).toBe(token);
+    });
+
+    it('basic token should be extracted correctly', () => {
+      const scheme = 'basic';
+      const token = '!@#$#abcd';
+      const result = authService.extractTokenFromHeader(`${scheme} ${token}`, 'basic');
+      expect(result).toBe(token);
+    });
+
+    it('Token Scheme should be case-insensitive', () => {
+      const scheme = 'Basic';
+      const token = '!@#$#abcd';
+      const result = authService.extractTokenFromHeader(`${scheme} ${token}`, `basic`);
+      expect(result).toBe(token);
+    });
+
+    it('Scheme Mismatch Should throw Error', () => {
+      const scheme = 'Bearer';
+      const token = '!@#$#abcd';
+      expect(() => authService.extractTokenFromHeader(`${scheme} ${token}`, 'basic')).toThrow(
+        BadRequestException
+      );
+    });
+
+    it('Authorization Header of Wrong Format Should throw Error', () => {
+      const scheme = 'Bearer';
+      const token = '!@#$#abcd';
+      expect(() => authService.extractTokenFromHeader(`${scheme} ${token} asdf`, 'bearer')).toThrow(
+        BadRequestException
+      );
+    });
   });
 });
